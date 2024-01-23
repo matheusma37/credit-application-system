224c9bd24db54c9f0fc955eddf5daa80bf37d8f8
matheusma37 <mtsm37@gmail.com>
2024-01-23 00:10:03 -0300

    Add CustomerController and DTOs

---
+++ src/main/kotlin/me/dio/credit/application/system/controller/CustomerController.kt
@@ -0,0 +1,40 @@
+package me.dio.credit.application.system.controller
+
+import me.dio.credit.application.system.dto.CustomerDTO
+import me.dio.credit.application.system.dto.CustomerUpdateDto
+import me.dio.credit.application.system.dto.CustomerView
+import me.dio.credit.application.system.entity.Customer
+import me.dio.credit.application.system.service.impl.CustomerService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/customers")
+class CustomerController(
+    private val customerService: CustomerService
+) {
+    @PostMapping
+    fun saveCustomer(@RequestBody() customerDto: CustomerDTO): String {
+        val savedCustomer = this.customerService.save(customerDto.toEntity())
+        return "Customer ${savedCustomer.email} saved!"
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        return CustomerView(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
+
+    @PatchMapping
+    fun updateCustomer(
+        @RequestParam(value = "customerId") id: Long,
+        @RequestBody customerUpdateDto: CustomerUpdateDto
+    ): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        val customerToUpdate: Customer = customerUpdateDto.toEntity(customer)
+        val customerUpdated: Customer = this.customerService.save(customerToUpdate)
+        return CustomerView(customerUpdated)
+    }
+}
